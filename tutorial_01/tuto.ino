63fe555eb7306dc88fb7514d47637d4f30776606
hw.lee <hyeongu0305@gmail.com>
2021-04-20 01:00:19 +0900

    vscode 에서 첫 구동

--- tutorial_01/tuto.ino
+++ tutorial_01/tuto.ino
@@ -1,10 +1,10 @@
-void setup()
-{
-    pinMode(13, OUTPUT);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
 }
- 
-void loop()
-{
-    digitalWrite(13, !(digitalRead(13)));
-    delay(2000);
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);                      
 }
